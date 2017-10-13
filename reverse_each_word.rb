2811157e1132915f811d5a2cd3441d024b8f67f4
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-10-13 19:00:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,9 @@ def reverse_each_word(string)
   a = string.split(" ").each { |word| word.reverse! }
   a.join(" ")
 end
+
+def reverse_each_word(string)
+  a = string.split(" ")
+  a.collect { |word| word.reverse! }
+  a.join(" ")
+end
